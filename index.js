67c0b38c1d4cffe9d7030a52090813b4c339c2a0
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2022-11-16 14:02:11 +0100

    push 16.11 14:02

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello, Node.JS!");
+#! /usr/bin/env node
+console.log ("test"); 
+
+// import axios from 'axios'; 
+
+// const root = "https://date.nager.at/api/v3/PublicHolidays/";
+
+// async function getApi(year, countryCode){
+
+//    let response = await axios.get(`${root}{${year}}/{${countryCode}}`)
+//     console.log (response); 
+// }
+// getApi(year, countryCode); 
+
+const program = require('commander');
+
+program
+    .version('1.0.0')
+    .command('klt')
+    .description('Take Country Name as parameter')
+    .option('short', 'Country Code') 
+    .option('year', 'Gives corresponding holidays')
+    .parse(process.argv);
+
+console.log(process.argv);
+
+// const year = process.argv[2];
+// const countryCode = process.argv[3];
