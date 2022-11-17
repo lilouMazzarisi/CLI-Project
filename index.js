27bdd7c29dffb2e7cc9a39ef6cb80e2607b845b9
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2022-11-17 10:58:43 +0100

    final push

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 import axios from 'axios';
 import chalk from 'chalk';
+import figlet from 'figlet';
+import ora from 'ora';
 import {getCode, getName} from 'country-list'; 
 
 const HolidaysAPIurl = "https://date.nager.at/api/v3/PublicHolidays/";
@@ -11,17 +13,35 @@ const fetchAllHolidays = async (year,countryCode) =>{
     return response.data;
 }
 
-console.log (chalk.green('test')); 
-// TRANSFORM SECOND ARGUMENT TO COUNTRY CODE
+//PUT "HOLIDAYS" BEFORE DATA 
+function callText(){
+    return console.log(figlet.textSync('Holidays', {
+        font: 'Ghost',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 100,
+        whitespaceBreak: true
+    }));
+}
 
+
+
+
+
+//LOADING SPINNER 
+const spinner = ora('Loading \n \n').start()
+
+spinner.color = 'blue';
 
 async function Holidaysloop(year,countryCode){
 
     const HolidaysData = await fetchAllHolidays(year,countryCode);
+    spinner.succeed('The holidays for the current year are : \n')
     for (const holiday of HolidaysData) {
-        let name = holiday.name;
-        let date = holiday.date;   
-        let display = date + ":" + name
+        let name = chalk.yellow(holiday.name);
+        let date = chalk.blue(holiday.date);  
+        let localName = chalk.magenta(holiday.localName); 
+        let display = date + ": " + name.padEnd(38) + " aka ".padEnd(8) + localName
         console.log(display); 
     }
 }
@@ -32,22 +52,21 @@ const displayHolidays = async () => {
     try{
         let countryCode = getCode(process.argv[2]); 
         const currentYear = new Date().getFullYear();
+        callText();
+        
         if(process.argv.length === 3){
-            
-            Holidaysloop(currentYear,countryCode);
-            
+            Holidaysloop(currentYear,countryCode);            
         }else{
             let givenYear = process.argv[3]; 
             Holidaysloop(givenYear,countryCode);
         }
-    
-        // console.log(HolidaysData.length);
-        // return(HolidaysData.name + HolidaysData.date);
     }
     catch(error){
-                console.log("NOOO");
+        spinner.fail('There was an issue with your input');
     }
 }
 
 displayHolidays();
+
+
 
